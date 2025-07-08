4750a8d0f9407fe63538ebb4ac2205afe5f3fea1
twsnmp <ymi@twise.co.jp>
2025-07-09 05:55:07 +0900

    Add get_network_list tool

--- tools/get_network_list.py
+++ tools/get_network_list.py
@@ -17,21 +17,20 @@ logger.addHandler(plugin_logger_handler)
 
 from twsnmpapi.twsnmpapi import TwsnmpAPI
 
-class GetNodeListTool(Tool):
+class GetNetworkListTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         """
-        TWSNMP FCからノードリストを取得してJSONで返す。
+        TWSNMP FCからネットワークリストを取得してJSONで返す。
 
         Args:
             tool_parameters: ツールの入力パラメータを含む辞書:
                 - name_filter (str): 名前フィルター
                 - ip_filter (str): IPフィルター
-                - state_filter (str): 状態フィルター
 
         Yields:
             ToolInvokeMessage: JSON
         """
-        logger.info("start get_node_list")
+        logger.info("start get_network_list")
         # 1. ランタイムから認証情報を取得
         try:
             url = self.runtime.credentials["twsnmp_url"]
@@ -44,7 +43,6 @@ class GetNodeListTool(Tool):
         # 2. ツールの入力パラメータを取得
         name = tool_parameters.get("name_filter", "") 
         ip = tool_parameters.get("ip_filter", "") 
-        state = tool_parameters.get("state_filter", "") 
 
         # 3. TWSNMP FCからノードリストを取得
         api = TwsnmpAPI(url)
@@ -53,19 +51,21 @@ class GetNodeListTool(Tool):
             logger.error("Can not login to TWSNMP FC")
             yield self.create_text_message(f"Can not login to TWSNMP FC: {r}")
           
-        nodes = api.get("/api/nodes")
-        if not nodes:
-            logger.error("Failed to retrieve nodes from TWSNMP FC")
-            yield self.create_text_message("Failed to retrieve nodes from TWSNMP FC")
+        map = api.get("/api/map")
+        if not map:
+            logger.error("Failed to retrieve map from TWSNMP FC")
+            yield self.create_text_message("Failed to retrieve map from TWSNMP FC")
+        networks = map.get("Networks",None)
+        if not networks:
+            logger.error("Failed to retrieve networks from TWSNMP FC")
+            yield self.create_text_message("Failed to retrieve networks from TWSNMP FC")
         
         # 4. 結果を返す
-        for node  in nodes:
-            if name and name not in node.get("Name",""):
+        for  key,network  in networks.items():
+            if name and name not in network.get("Name",""):
                 continue
-            if ip and ip not in node.get("IP",""):
+            if ip and ip not in network.get("IP",""):
                 continue
-            if state and state not in node.get("State",""):
-                continue
-            yield self.create_json_message(node)
-        logger.info("end get_node_list")
+            yield self.create_json_message(network)
+        logger.info("end get_network_list")
  
